8ad05e0ddc49d2e7a62c4f9840117916e1fc7843
Ahmed <siam684@gmail.com>
2014-05-08 23:14:19 -0400

    update

--- src/os/project/FreeSpaceNode.java
+++ src/os/project/FreeSpaceNode.java
@@ -1,5 +1,50 @@
 package os.project;
 
-public class FreeSpaceNode {
-
+public class FreeSpaceNode implements Comparable<FreeSpaceNode>
+{
+	private int address;
+	private int size;
+	
+	FreeSpaceNode(int address, int size)
+	{
+		 this.address = address;
+		 this.size = size;
+	}
+	
+	int getSize()
+	{
+		return size;
+	}
+	
+	int getAddress()
+	{
+		return address;
+	}
+	
+	void setAddress(int address)
+	{
+		this.address = address;
+	}
+	
+	void setSize(int size)
+	{
+		this.size = size;
+	}
+	
+	@Override
+	public int compareTo(FreeSpaceNode node) 
+	{
+		if (this.address > node.address) 
+		{
+			return 1;
+		} 
+		else if (this.address == node.address) 
+		{
+			return 0;
+		} 
+		else 
+		{
+			return -1;
+		}
+	}
 }
--- src/os/project/OS.java
+++ src/os/project/OS.java
@@ -1,5 +1,91 @@
 package os.project;
+import java.util.LinkedList;
 
-public class OS {
+public class OS 
+{
+	
+	LinkedList<FreeSpaceNode> freeSpaceTable;
+	FreeSpaceNode outNode;
+	
+	void startup()
+	{
+		freeSpaceTable = new LinkedList<FreeSpaceNode>();
+		freeSpaceTable.add(new FreeSpaceNode(0,100));
+	}
+	
+	void Crint(int a[],int p[])
+	{
+		
+	}
+	
+	void Dskint(int a[],int p[])
+	{
+		
+	}
+	
+	void Drmint(int a[],int p[])
+	{
+		
+	}
+	
+	void Tro(int a[],int p[])
+	{
+		
+	}
+	
+	void Svc(int a[],int p[])
+	{
+		
+	}
+	
+	/*
+	 * 
+	 * */
+	void memManage(int sizeOfJob)
+	{
+		int sentinal = 1;
+		while(freeSpaceTable.peek().getSize()<sizeOfJob&&sentinal<=freeSpaceTable.size())
+		{
+			freeSpaceTable.push(freeSpaceTable.pop());
+			sentinal++;
+		}
+		memSplit(sizeOfJob);
+		//call to swapper to swap job drum->mem 
+		}else
+		{
+			//choose algorithm to replace job
+			
+		}
+	}
+	
+	void cpuSchedule()
+	{
+		
+	}
+	
+	void dispatch()
+	{
+		
+	}
+	
+	
+	/*
+	 * swaps jobs between drum and memory
+	 * */
+	void swap()
+	{
+		
+	}
 
+	void memCoalesce()
+	{
+		
+	}
+	
+	void memSplit(int sizeOfJob)
+	{
+		outNode = freeSpaceTable.remove();
+		freeSpaceTable.add(new FreeSpaceNode(outNode.getAddress()+sizeOfJob,outNode.getSize()-sizeOfJob));
+	}
+	
 }
